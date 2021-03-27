4b28e70eb33a825c0ff53e3787ccef3abced6394
Vitalii Stroinov <stroinov.vitalii@gmail.com>
2021-03-27 17:18:12 +0200

    readme.md

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
 	console.log('Server started');
 });
